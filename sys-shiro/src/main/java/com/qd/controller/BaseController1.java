e121b04c025ea808e98faab578377edb765d69e8
cbyZing <849650806>
2017-04-16 13:10:07 +0800

    maven多模块

---
+++ sys-shiro/src/main/java/com/qd/controller/BaseController1.java
@@ -0,0 +1,20 @@
+package com.qd.controller;
+
+import com.qd.entity.SysUserEntity;
+import com.qd.utils.ShiroUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Controller公共组件
+ *
+ */
+public abstract class BaseController1 {
+    protected Logger logger = LoggerFactory.getLogger(getClass());
+
+    protected SysUserEntity getUser() {
+        return ShiroUtils.getUserEntity();
+    }
+
+    //protected Long getUserId() {//return getUser().getUserId();}
+}
